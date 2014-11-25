34d3a38b723b3f821a1967a64df65325d7cbe200
MaxRobinson <maxrgit@gmail.com>
2014-11-24 16:49:08 -0800

    Addition of a PhaseStateTest Class for Testing
    Class for testing phase state.

---
+++ Phase10Test/src/edu/up/cs301/game/test/PhaseStateTest.java
@@ -0,0 +1,23 @@
+package edu.up.cs301.game.test;
+
+import android.util.Log;
+import edu.up.cs301.card.Card;
+import edu.up.cs301.phase10.Deck;
+import edu.up.cs301.phase10.Hand;
+import edu.up.cs301.phase10.PhaseMainActivity;
+import edu.up.cs301.phase10.PhaseState;
+
+public class PhaseStateTest extends android.test.ActivityInstrumentationTestCase2<PhaseMainActivity> {
+	
+	
+	public PhaseStateTest(){
+		super(PhaseMainActivity.class);
+	}
+	
+	// This is not needed for the way that we are testing
+	// Leaving blank so that it shows this was done intentionaly.
+	public void setUp(){
+	}
+	
+	
+}
